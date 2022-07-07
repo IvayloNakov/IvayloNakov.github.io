57875a7d52eedb8667e567995765c16d3ade740b
IvayloNakov <108884085+IvayloNakov@users.noreply.github.com>
2022-07-07 17:16:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,56 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs/promises');
 
-
-const url = 'https://blog.apify.com/';
-var cards
-
-const selector = ".post-card__content";
-
 (async function() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+  //setting up the headless browsere
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://blog.apify.com/');
+  
+  //accepting cookies alert
+  try {
+    await page.waitForSelector("#onetrust-accept-btn-handler", {timeout: 5000});
+    await page.click("#onetrust-accept-btn-handler")
+  } catch (error) {
+    if (error instanceof puppeteer.errors.TimeoutError) {
+      console.log('no alert')
+    }
+  }
+  
+  // main loop loads all pages and then scrapes data
     try {
-        await page.waitForSelector("#onetrust-accept-btn-handler", {
-            timeout: 5000
-        });
-        await page.click("#onetrust-accept-btn-handler")
-    } catch (e) {
-        if (e instanceof puppeteer.errors.TimeoutError) {
-            console.log('no alert')
-        }
+      //trying to load as many pages as possible
+      await page.waitForFunction("!document.querySelector('.btn').className.includes(' is-loading')")
+      await page.waitForSelector('.btn', {visible: true, timeout: 5000})
+      await page.click('.btn')
+
+    } catch (error) {
+      //on error means it cannot load new pages, so the scraping begins
+      cards = await page.$$eval(".post-card__content", nodes => {
+        return nodes.map(node => {
+          const tags = node.querySelector(".post-card__hdr").innerText.split("\n").join();
+          const title = node.querySelector('h2').innerText;
+          const authors = node.querySelector('.post-card__author').innerText.split("\n").join();
+          const discription = node.querySelector('.post-card__exc').innerText;
+          const date = node.querySelector('time').dateTime;
+          const url = node.querySelector('h2 a').href;
+
+          return {
+            tags,
+            title,
+            authors,
+            discription,
+            date,
+            url
+          }
+        })
+      });
+      break
     }
-    c = 1
-    while (true) {
-        try {
-            console.log(c++);
-            await page.waitForFunction("!document.querySelector('.btn').className.includes(' is-loading')")
-            await page.waitForSelector('.btn', { visible: true, timeout: 5000 })
-            await page.click('.btn')
+  }
+  await browser.close();
 
-
-        } catch (error) {
-            console.log("collecting cards");
-            cards = await page.$$eval(selector, nodes => {
-                return nodes.map(node => {
-                    const tags = node.querySelector(".post-card__hdr").innerText.split("\n").join();
-                    const title = node.querySelector('h2').innerText;
-                    const authors = node.querySelector('.post-card__author').innerText.split("\n").join();
-                    const discription = node.querySelector('.post-card__exc').innerText;
-                    const date = node.querySelector('time').dateTime;
-                    const url = node.querySelector('h2 a').href;
-
-                    return {
-                        tags,
-                        title,
-                        authors,
-                        discription,
-                        date,
-                        url
-                    }
-                })
-            });
-            break
-        }
-    }
-    await browser.close();
-
-    await fs.writeFile('C_A_R_D_S.txt', JSON.stringify(cards))
+  //saves the JSON data as a text file on the disk
+  await fs.writeFile('AplifyBlog_scraped.txt', JSON.stringify(cards))
 
 })();
